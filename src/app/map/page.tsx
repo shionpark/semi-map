7972d74611d3a3e032e0abe2366711c7941794e7
Shion <seoy1108@gmail.com>
2025-07-16 16:31:54 +0900

    style: 카카오맵 layout에서 별도 컴포넌트로 변경
    - 테스트 에러 해결 시도

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import KakaoMap from "@/components/KakaoMap";
 import Link from "next/link";
 
 export default function Map() {
   return (
     <>
+      <KakaoMap />
       <Link href={"/map/1"}>1</Link>
       <Link href={"/map/2"}>2</Link>
       <Link href={"/map/3"}>3</Link>
--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -4,13 +4,12 @@ import Script from "next/script";
 import { useInitializeKakaoMap } from "@/hooks/useInitializeKakaoMap";
 import { styles } from "@/styles/index";
 
-export default function MapLayout({ children }: { children: React.ReactNode }) {
+export default function KakaoMap() {
   const { mapRef, sdkLoadSuccess, sdkLoadFail } = useInitializeKakaoMap();
 
   return (
     <>
-      <div ref={mapRef} className={styles.map} />
-      {children}
+      <div id="map" data-testid="map" ref={mapRef} className={styles.map} />
       <Script
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY}&libraries=services&autoload=false`}
         onLoad={() => sdkLoadSuccess()}
