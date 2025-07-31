46c4501590bfc11070027980684b9767adb304b0
Shion <seoy1108@gmail.com>
2025-07-31 14:59:24 +0900

    chore: pre-commit과 pre-push 훅 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,6 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-amber-100">
       <h1>Semi Map</h1>
       <Link href={'/map'}>지도</Link>
-      <div>test</div>
-      <div>test1</div>
-      <div>test2</div>
-      <div>test3</div>
     </div>
   );
 }
