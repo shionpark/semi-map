37c34d11bdc87ffb2db40935577ec3fe38f3117c
Shion <seoy1108@gmail.com>
2025-08-05 13:47:47 +0900

    refactor: 커스텀 오버레이 리액트 컴포넌트로 변경 및 초기 지도 생성 함수 분리
    - closeOverlay 함수 prop 전달 및 버튼에 연결

--- src/hooks/useInitializeKakaoMap.ts
+++ src/hooks/useInitializeKakaoMap.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-import { createCustomOverlay } from '@/utils/kakao.utils';
+import { createCustomOverlay, createInitialMap } from '@/utils/kakao.utils';
 import { useKakaoSdkStore } from '@/store/useKakaoSdkStore';
 
 export function useInitializeKakaoMap() {
@@ -11,20 +11,7 @@ export function useInitializeKakaoMap() {
     if (!isLoaded || !window.kakao?.maps || !mapRef.current) return;
 
     window.kakao.maps.load(() => {
-      const currentPosition = new window.kakao.maps.LatLng(
-        33.450701,
-        126.570667
-      );
-
-      const map = new window.kakao.maps.Map(mapRef.current, {
-        center: currentPosition,
-        level: 3,
-      });
-
-      const marker = new window.kakao.maps.Marker({
-        map,
-        position: currentPosition,
-      });
+      const { map, marker } = createInitialMap(mapRef);
 
       createCustomOverlay(map, marker);
     });
